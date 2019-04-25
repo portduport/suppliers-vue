973e3564cd1a8398803657e9c926ecaf915d4ce8
portduport <pablo@callejo.fr>
2019-04-25 13:58:37 +0200

    Step 3 finished

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <h1>Que voulez vous faire ?</h1>
+        <button type="button" class="btnhome" id="liste" @click="onSuppliersListClick">Consulter la liste des fournisseurs</button>
+        <button type="button" class="btnhome" id="carte" @click="onMapClick">Afficher la carte</button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import SuppliersList from './components/SuppliersList.vue'
+    import SuppliersMap from './components/SuppliersMap.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            SuppliersList,
+            SuppliersMap
+        },
+        methods:{
+            onSuppliersListClick: function () {
+                window.alert("Clicked!!");
+            },
+            onMapClick: function () {
+                window.alert("Clicked!!");
+            }
+
+        }
+
+    }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    .btnhome{
+        background-color: green;
+        padding: 15px 15px;
+        margin: 0 5px;
+        color: white;
+        font-weight: 900;
+        font-size: 0.8em;
+        cursor: pointer;
+    }
+
 </style>
